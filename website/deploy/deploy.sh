a196f1599127f542f5547f66a70896894929fb78
Scott W Harden <swharden@gmail.com>
2021-02-11 19:22:02 -0500

    update deployment script

--- website/deploy/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# This script pulls the latest website from GitHub.
-# - This file should be read-only
-# - This file should be outside your web folder
-# - Call from PHP via system() not exec()
-# - The PHP script calling this should require authentication
-
-git pull
-git status
--- website/deploy/index.php
+++ website/deploy/index.php
@@ -1,23 +1,26 @@
 <?php
 
-// Ensure root files are accessible
-if (file_exists('/home/customer/pull.key') == false) {
-    echo "ERROR: pull.key does not exist";
+// Ensure key file exists (though it should not be web-accessible)
+$keyFilePath = realpath('../../../../../../') . '/deploy.key';
+if (file_exists($keyFilePath) == false) {
+    echo "ERROR: file not found $keyFilePath";
     die();
 }
 
 // Get authorization header from Apache
-$headers = apache_request_headers();
-if (!isset($headers['Authorization'])) {
-    echo "ERROR: Authorization Required";
+$pass = $_SERVER['PHP_AUTH_PW'];
+if (empty($pass)){
+    echo "ERROR: Password Required";
     die();
 }
 
 // Compare given token vs one on disk
-$givenToken = substr($headers['Authorization'], 7);
-$realToken = trim(file_get_contents('/home/customer/pull'));
-if ($givenToken == $realToken) {
-    system('deploy.sh');
+$correctPassword = trim(file_get_contents($keyFilePath));
+if ($pass == $correctPassword) {
+	echo "PULL:\n";
+    echo exec('git pull');
+	echo "\n\nSTATUS:\n";
+    echo exec('git status');
 } else {
     echo "ERROR: Authorization Failed";
 }
