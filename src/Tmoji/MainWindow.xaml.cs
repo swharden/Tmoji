45e3284c45b00519e82d4cbcf881231540637e40
Scott W Harden <swharden@gmail.com>
2021-02-12 20:51:00 -0500

    registry option to launch with Windows

--- src/Tmoji/MainWindow.xaml.cs
+++ src/Tmoji/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ namespace Tmoji
             InitializeComponent();
             Version version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
             AboutMenuItem.Header = $"About Tmoji {version.Major}.{version.Minor}";
+            StartWithWindowsMenuItem.IsChecked = StartWithWindowsIsEnabled();
             TrayIcon_Init();
             ResetLayout();
             Hide();
@@ -145,6 +146,51 @@ namespace Tmoji
         {
             string url = "https://github.com/swharden/Tmoji";
             Process.Start(url);
+        }
+
+        private void Button_Menu_ToggleStartWithWindows(object sender, RoutedEventArgs e)
+        {
+            var menuItem = sender as MenuItem;
+            StartWithWindows(enable: menuItem.IsChecked);
+        }
+
+        const string RUN_PATH = "Software\\Microsoft\\Windows\\CurrentVersion\\Run";
+        const string RUN_VALUE_NAME = "Tmoji";
+
+        private string TmojiExePath() => 
+            System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+        private bool StartWithWindowsIsEnabled()
+        {
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(RUN_PATH, true);
+            if (key is null)
+                return false;
+
+            var val = key.GetValue(RUN_VALUE_NAME);
+            if (val is null)
+                return false;
+
+            return val.ToString() == TmojiExePath();
+        }
+
+        private void StartWithWindows(bool enable)
+        {
+            var key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey(RUN_PATH, true);
+
+            if (enable)
+            {
+                if (key is null)
+                    Microsoft.Win32.Registry.CurrentUser.CreateSubKey(RUN_PATH);
+
+                key.SetValue(
+                    name: RUN_VALUE_NAME,
+                    value: TmojiExePath(),
+                    valueKind: Microsoft.Win32.RegistryValueKind.String);
+            }
+            else
+            {
+                key.DeleteValue("Tmoji", throwOnMissingValue: false);
+            }
         }
     }
 }
